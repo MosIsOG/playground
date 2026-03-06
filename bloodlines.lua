15bd4f05fd6bd0b98e88f4571d259159c71f5847
root <root@DESKTOP-9SJE40H.localdomain>
2026-03-06 03:45:26 -0500

    minter

--- bloodlines.lua
+++ bloodlines.lua
@@ -2922,6 +2922,7 @@ local BlockRules = {
    { animID = "6360969229", delay = 0.18, distance = 15 },
    { animID = "11330795390", delay = 0.115, distance = 6 },
    { animID = "7275651023", delay = 0.2, distance = 19 },
+   { animID = "86213040968703", delay = 0.0, distance = 25, continuous = true },
 }
 
 -- Test rule (temporary)
